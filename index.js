f55610620c4f097e9844a5c1c59320d88767b1b5
cygg <46070794+chinaliyue@users.noreply.github.com>
2025-04-15 18:09:30 +0800

    Update index.js
    优化代码结构，添加了部分网站。

--- index.js
+++ index.js
@@ -2,10 +2,9 @@
  *  自定义网站配置 
  */
 const config = {
-  title: "自定义导航",                 //write your website title
-  subtitle: "Cloudflare Workers Dir", //write your website subtitle
+  title: "自定义导航",                 // 网站标题，显示在页面头部和浏览器标签中
+  subtitle: "Cloudflare Workers Dir", // 网站副标题，显示在页面头部
   logo_icon: "sitemap",               //select your logo by semantic-ui icon (you can get more msg in:https://semantic-ui.com/elements/icon.html)
-  hitokoto: true,                     //use hitokoto or not
   search:true,                        //enable search function
   search_engine:[                     //choose search engine which you use
     {
@@ -17,81 +16,131 @@ const config = {
       template:"https://www.google.com/search?q=$s"
     },
     {
-      name:"必 应",
-      template:"https://www.bing.com/search?q=$s"
+      name:"GitHub",
+      template:"https://github.com/search?q=$s&type=repositories"
     },
     {
-      name:"搜 狗",
-      template:"https://www.sogou.com/web?query=$s"
+      name:"liteapks",
+      template:"https://liteapks.com/?s=$s"
     }
   ],
-  selling_ads: true,                  //Selling your domain or not.(turning on may be helpful for selling this domain by showing some ads.)
-  sell_info:{
-    domain:"example.com",
-    price:500,                        //domain price
-    mon_unit:"yen sign",              //monetary unit 
-    contact:[                         //how to contact you
-      {
-        type:"envelope",               //contact type ("weixin","qq","telegram plane","envelope" or "phone")
-        content:"info@example.com"
-      }
-    ]                        
-  },
   lists: [                            //Url list
     {
-      name:"技术",
-      icon:"code",
+      name:"信息收集",
+      icon:"globe",
       list:[
         {
-          url:"https://oschina.net/",
-          name:"开源中国",
-          desc:"程序员集散地"
+          url:"https://www.whitehouse.gov/",
+          name:"White House",
+          desc:"美国白宫官方网站"
         },
         {
-          url:"https://v2ex.com",
-          name:"V2EX",
-          desc:"程序员集散地"
+          url:"https://www.gov.uk/",
+          name:"GOV.UK",
+          desc:"英国政府官方网站"
         },
         {
-          url:"https://csdn.net/",
-          name:"CSDN技术社区",
-          desc:"程序员集散地"
+          url:"https://www.gouvernement.fr/",
+          name:"Gouvernement",
+          desc:"法国政府官方网站"
         },
         {
-          url:"https://github.com/",
-          name:"Github",
-          desc:"程序员集散地"
+          url:"https://www.bundesregierung.de/",
+          name:"Bundesregierung",
+          desc:"德国联邦政府官方网站"
         },
+        {
+          url:"https://www.gov.cn/",
+          name:"中国政府网",
+          desc:"中华人民共和国中央人民政府"
+        },
+        {
+          url:"https://japan.kantei.go.jp/",
+          name:"首相官邸",
+          desc:"日本首相官邸官方网站"
+        }
       ]
     },
     {
-      name:"学习",
-      icon:"graduation cap",
+      name:"数据公布",
+      icon:"chart line",
       list:[
         {
-          url:"https://w3school.com.cn/",
-          name:"W3school在线教程",
-          desc:"程序员集散地"
+          url:"https://www.bea.gov/",
+          name:"BEA",
+          desc:"美国经济分析局"
         },
         {
-          url:"https://runoob.com/",
-          name:"菜鸟教程",
-          desc:"程序员集散地"
+          url:"https://www.ons.gov.uk/",
+          name:"ONS",
+          desc:"英国国家统计局"
         },
         {
-          url:"https://segmentfault.com/",
-          name:"思否社区",
-          desc:"程序员集散地"
+          url:"https://www.insee.fr/",
+          name:"INSEE",
+          desc:"法国国家统计与经济研究所"
         },
         {
-          url:"https://jianshu.com/",
-          name:"简书",
-          desc:"程序员集散地"
+          url:"https://www.destatis.de/",
+          name:"Destatis",
+          desc:"德国联邦统计局"
         },
+        {
+          url:"http://www.stats.gov.cn/",
+          name:"国家统计局",
+          desc:"中国国家统计局"
+        },
+        {
+          url:"https://www.stat.go.jp/",
+          name:"日本统计局",
+          desc:"日本总务省统计局"
+        }
+      ]
+    },
+    {
+      name:"新闻网站",
+      icon:"newspaper",
+      list:[
+        {
+          url:"https://www.bbc.com/news",
+          name:"BBC News",
+          desc:"英国广播公司新闻"
+        },
+        {
+          url:"https://www.cnn.com/",
+          name:"CNN",
+          desc:"美国有线电视新闻网"
+        },
+        {
+          url:"https://www.reuters.com/",
+          name:"Reuters",
+          desc:"路透社"
+        },
+        {
+          url:"https://www.ft.com/",
+          name:"Financial Times",
+          desc:"英国金融时报"
+        },
+        {
+          url:"https://www.economist.com/",
+          name:"The Economist",
+          desc:"经济学人"
+        },
+        {
+          url:"https://www.nytimes.com/",
+          name:"The New York Times",
+          desc:"纽约时报"
+        },
+        {
+          url:"https://www.wsj.com/",
+          name:"The Wall Street Journal",
+          desc:"华尔街日报"
+        }
       ]
     }
   ]
-}
+};
+
 const el = (tag, attrs, content) => `<${tag} ${attrs.join(" ")}>${content}</${tag}>`;
 
 async function handleRequest(request) {
@@ -99,82 +148,68 @@ async function handleRequest(request) {
     headers: {
       'content-type': 'text/html;charset=UTF-8',
     },
-  }
-  return new Response(renderHTML(renderIndex(),config.selling_ads? renderSeller() :null), init);
+  };
+  return new Response(renderHTML(renderIndex()), init);
 }
+
 addEventListener('fetch', event => {
-  return event.respondWith(handleRequest(event.request))
-})
+  event.respondWith(handleRequest(event.request));
+});
 
-/*通过分析链接 实时获取favicon
-* @url 需要分析的Url地址
-*/
-function getFavicon(url){
-  if(url.match(/https{0,1}:\/\//)){
-    //return "https://ui-avatars.com/api/?bold=true&size=36&background=0D8ABC&color=fff&rounded=true&name=" + url.split('//')[1];
-    return "https://www.google.cn/s2/favicons?sz=64&domain_url=" + url;
-  }else{
-    //return "https://ui-avatars.com/api/?bold=true&size=36&background=0D8ABC&color=fff&rounded=true&name=" + url;
-    return "https://www.google.cn/s2/favicons?sz=64&domain_url=http://" + url;
-  } 
+/* 通过分析链接实时获取 favicon */
+function getFavicon(url) {
+  const domain = url.match(/https?:\/\/([^/]+)/)?.[1] || url;
+  return `https://www.google.com/s2/favicons?sz=64&domain=${domain}`;
 }
 
-/** Render Functions
- *  渲染模块函数
- */
-
-function renderIndex(){
-  const footer = el('footer',[],el('div',['class="footer"'],'Powered by' + el('a',['class="ui label"','href="https://github.com/sleepwood/cf-worker-dir"','target="_blank"'],el('i',['class="github icon"'],"") + 'Cf-Worker-Dir') + ' &copy; Base on ' + el('a',['class="ui label"'],el('i',['class="balance scale icon"'],"") + 'MIT License')));
+/** Render Functions */
+function renderIndex() {
+  const footer = el('footer', [], el('div', ['class="footer"'], 
+    'Powered by ' + el('a', ['class="ui label"', 'href="https://github.com/sleepwood/cf-worker-dir"', 'target="_blank"'], 
+    el('i', ['class="github icon"'], "") + 'Cf-Worker-Dir') + 
+    ' &copy; Base on ' + el('a', ['class="ui label"'], el('i', ['class="balance scale icon"'], "") + 'MIT License')));
   return renderHeader() + renderMain() + footer;
 }
 
-function renderHeader(){
-  const item = (template,name) => el('a',['class="item"',`data-url="${template}"`],name);
-
-  var nav = el('div',['class="ui large secondary inverted menu"'],el('div',['class="item"'],el('p',['id="hitokoto"'],'条条大路通罗马')))
-  var title = el('h1',['class="ui inverted header"'],el('i',[`class="${config.logo_icon} icon"`],"") + el('div',['class="content"'],config.title + el('div',['class="sub header"'],config.subtitle)));
-  var menu = el('div',['id="sengine"','class="ui bottom attached tabular inverted secondary menu"'],el('div',['class="header item"'],'&nbsp;') + config.search_engine.map((link,key) =>{
-    if(key == 0){
-      return el('a',['class="active item"',`data-url="${link.template}"`],link.name);
-    }else{
-      return item(link.template,link.name);
-    }
-  }).join(""))
-  var input = el('div',['class="ui left corner labeled right icon fluid large input"'],el('div',['class="ui left corner label"'],el('img',['id="search-fav"','class="left floated avatar ui image"','src="https://www.baidu.com/favicon.ico"'],"")) + el('input',['id="searchinput"','type="search"','placeholder="搜索你想要知道的……"','autocomplete="off"'],"") + el('i',['class="inverted circular search link icon"'],""));
-  return el('header',[],el('div',['id="head"','class="ui inverted vertical masthead center aligned segment"'],(config.hitokoto ? el('div',['id="nav"','class="ui container"'],nav) : "") + el('div',['id="title"','class="ui text container"'],title + (config.search ? input + menu :"") + `${config.selling_ads ? '<div><a id="menubtn" class="red ui icon inverted button"><i class="heart icon"></i> 喜欢此域名 </a></div>' : ''}`)))
+function renderHeader() {
+  const item = (template, name) => el('a', ['class="item"', `data-url="${template}"`], name);
+  const title = el('h1', ['class="ui inverted header"'], 
+    el('i', [`class="${config.logo_icon} icon"`], "") + 
+    el('div', ['class="content"'], config.title + el('div', ['class="sub header"'], config.subtitle)));
+  const menu = el('div', ['id="sengine"', 'class="ui bottom attached tabular inverted secondary menu"', 'style="z-index: 1;"'], 
+    el('div', ['class="header item"'], '&nbsp;') + config.search_engine.map((link, key) => 
+      el('a', [`class="${key === 0 ? 'active' : ''} item"`, `data-url="${link.template}"`], link.name)
+    ).join(""));
+  const input = el('div', ['class="ui left corner labeled right icon fluid small input"', 'style="z-index: 2;"'], 
+    el('div', ['class="ui left corner label"'], 
+      el('img', ['id="search-fav"', 'class="left floated avatar ui image"', 'src="https://www.baidu.com/favicon.ico"'], "")) + 
+    el('input', ['id="searchinput"', 'type="search"', 'placeholder="搜索你想要知道的……"', 'autocomplete="off"'], "") + 
+    el('i', ['class="inverted circular search link icon"'], ""));
+  const titleBelowSearch = el('div', ['class="ui text container"'], title);
+  return el('header', [], el('div', [
+    'id="head"', 
+    'class="ui inverted vertical masthead center aligned segment"', 
+    'style="max-width: 1200px; margin: 0 auto; height: 230px; padding: 0;"' // 控制头部高度和布局
+  ], 
+    el('div', ['id="title"', 'class="ui text container"'], (config.search ? input + menu + titleBelowSearch : ""))));
 }
 
 function renderMain() {
-  var main = config.lists.map((item) => {
-    const card = (url,name,desc)=> el('a',['class="card"',`href=${url}`,'target="_blank"'],el('div',['class="content"'],el('img',['class="left floated avatar ui image"',`src=${getFavicon(url)}`],"") + el('div',['class="header"'],name) + el('div',['class="meta"'],desc)));
-    const divider = el('h4',['class="ui horizontal divider header"'],el('i',[`class="${item.icon} icon"`],"")+item.name);
-
-    var content = el('div',['class="ui four stackable cards"'],item.list.map((link) =>{
-      return card(link.url,link.name,link.desc);
-    }).join(""));
-
-    return el('div',['class="ui basic segment"'],divider + content);
-  }).join("");
-  
-  return el('main',[],el('div',['class="ui container"'],main));
+  return el('main', [], el('div', ['class="ui container"'], config.lists.map(item => {
+    const card = (url, name, desc) => el('a', ['class="card"', `href="${url}"`, 'target="_blank"'], 
+      el('div', ['class="content"'], 
+        el('img', ['class="left floated avatar ui image"', `src="${getFavicon(url)}"`], "") + 
+        el('div', ['class="header"'], name) + 
+        el('div', ['class="meta"'], desc)));
+    const divider = el('h4', ['class="ui horizontal divider header"'], 
+      el('i', [`class="${item.icon} icon"`], "") + item.name);
+    const content = el('div', ['class="ui four stackable cards"'], item.list.map(link => 
+      card(link.url, link.name, link.desc)).join(""));
+    return el('div', ['class="ui basic segment"'], divider + content);
+  }).join("")));
 }
 
-function renderSeller() {
-  const item = (type,content) => el('div',['class="item"'],el('i',[`class="${type} icon"`],"") + el('div',['class="content"'],content));
-  var title = el('h1',['class="ui yellow dividing header"'],el('i',['class="gem outline icon"'],"") + el('div',['class="content"'],config.sell_info.domain + ' 正在出售'));
-  var action = el('div',['class="actions"'],el('div',['class="ui basic cancel inverted button"'],el('i',['class="reply icon"'],"") + '返回'));
-
-  var contact = config.sell_info.contact.map((list) => {
-    return item(list.type,list.content);
-  }).join("");
-  var column = el('div',['class="column"'],el('h3',['class="ui center aligned icon inverted header"'],el('i',['class="circular envelope open outline grey inverted icon"'],"") + '联系我') + el('div',['class="ui relaxed celled large list"'],contact));
-  var price = el('div',['class="column"'],el('div',['class="ui large yellow statistic"'],el('div',['class="value"'],el('i',[`class="${config.sell_info.mon_unit} icon"`],"") + config.sell_info.price)));
-  var content = el('div',['class="content"'],el('div',['class="ui basic segment"'],el('div',['class="ui two column stackable center aligned grid"'],el('div',['class="ui inverted vertical divider"'],'感兴趣？') + el('div',['class="middle aligned row"'],price + column))));
-
-  return el('div',['id="seller"','class="ui basic modal"'],title + content + action);
-}
-
-function renderHTML(index,seller) {
+function renderHTML(index) {
   return `<!DOCTYPE html>
   <html lang="en">
   <head>
@@ -189,31 +224,22 @@ function renderHTML(index,seller) {
   </head>
   <body>
     ${index}
-    ${config.selling_ads ? seller : ''}
-    <script src="https://v1.hitokoto.cn/?encode=js&select=%23hitokoto" defer></script>
     <script>
-      $('#sengine a').on('click', function (e) {
-        $('#sengine a.active').toggleClass('active');
-        $(e.target).toggleClass('active');
-        $('#search-fav').attr('src',$(e.target).data('url').match(`+/https{0,1}:\/\/\S+\//+`)[0] + '/favicon.ico') ;
-      });
-      $('.search').on('click', function (e) {
-          var url = $('#sengine a.active').data('url');
-          url = url.replace(`+/\$s/+`,$('#searchinput').val());
+      $(document).ready(function () {
+        $('#sengine a').on('click', function () {
+          $('#sengine a.active').removeClass('active');
+          $(this).addClass('active');
+          $('#search-fav').attr('src', new URL($(this).data('url')).origin + '/favicon.ico');
+        });
+        $('.search').on('click', function () {
+          const url = $('#sengine a.active').data('url').replace('$s', $('#searchinput').val());
           window.open(url);
-      });
-      /* 鼠标聚焦时，回车事件 */
-      $("#searchinput").bind("keypress", function(){
-          if (event.keyCode == 13){
-          // 触发需要调用的方法
-          $(".search").click();
-          }
-      });
-      $('#menubtn').on('click', function (e) {
-          $('#seller').modal('show');
+        });
+        $('#searchinput').on('keypress', function (e) {
+          if (e.keyCode === 13) $('.search').click();
+        });
       });
     </script>
   </body>
-
-  </html>`
+  </html>`;
 }
